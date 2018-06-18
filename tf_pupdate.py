70da2385274d9b555d0f0e1492cafa507383ed35
Arnvid Karstad <arnvid.karstad@basefarm.com>
2018-06-18 09:38:08 +0200

    Added support for upgrading Terraform binaries

--- tf_pupdate.py
+++ tf_pupdate.py
@@ -8,7 +8,7 @@ from lxml import html
 import urllib.request
 import zipfile
 
-tfver = 0.1
+tfver = 0.5
 debug = False
 p_os      = sys.platform
 p_arch   = platform.architecture()[0]
@@ -31,6 +31,8 @@ config.sections()
 tmp_dir =  os.path.expanduser(config['PATHS'].get('tmp_dir'))
 plugins_dir = os.path.expanduser(config['PATHS'].get('plugins_dir'))
 plugins_dir = plugins_dir+"/"+p_os+"_"+p_tfarch
+
+tf_dir = config['PATHS'].get('tf_dir')
 
 if debug : print(tmp_dir)
 if debug : print(plugins_dir)
@@ -78,7 +80,7 @@ for option in config.options('PROVIDERS'):
     else:
       print("Local file does not exist:",tfp_lfilename)
       if os.path.isfile(tfp_lzippath): 
-         print("Local zip file does exist: "+tfp_filename+"_"+p_os+"_"+p_tfarch+".zip")
+        print("Local zip file does exist: "+tfp_filename+"_"+p_os+"_"+p_tfarch+".zip")
       else:
         print("Local zip file does not exist: "+tfp_filename+"_"+p_os+"_"+p_tfarch+".zip")
         urllib.request.urlretrieve(tfp_url, tfp_lzippath)
@@ -86,4 +88,38 @@ for option in config.options('PROVIDERS'):
       zfile.extract(tfp_lfilename,plugins_dir)
       zfile.close
       os.chmod(tfp_lfile, 0o755)
+
+tf_url = release_url+"terraform/"
+if debug : print(tf_url)
+webcontent = requests.get(tf_url)
+if debug : print(webcontent)
+html_content = html.fromstring(webcontent.content)
+nodes = html_content.xpath('/html/body/ul/li/a/text()')
+tfp_filename=nodes[1]
+tfp_version=remove_prefix(tfp_filename, "terraform_")
+tfp_lfilename="terraform_"+tfp_version+""
+tfp_url = tf_url+tfp_version+"/"+tfp_filename+"_"+p_os+"_"+p_tfarch+".zip"
+tfp_lzippath = tmp_dir+"/"+tfp_filename+"_"+p_os+"_"+p_tfarch+".zip"
+if debug : print(tfp_url)
+if debug : print(tfp_lfilename)
+tfp_lfile = Path(tf_dir+"/"+tfp_lfilename)
+if debug : print(tfp_lfile)
+if os.path.isfile(tfp_lfile):
+  print("Local file exists:",tfp_lfilename,"not upgrading")
+else:
+ print("Local file does not exist:",tfp_lfilename)
+ if os.path.isfile(tfp_lzippath):
+    print("Local zip file does exist: "+tfp_filename+"_"+p_os+"_"+p_tfarch+".zip")
+ else:
+    print("Local zip file does not exist: "+tfp_filename+"_"+p_os+"_"+p_tfarch+".zip")
+    urllib.request.urlretrieve(tfp_url, tfp_lzippath)
+
+ zfile = zipfile.ZipFile(tfp_lzippath)
+ zfile.extract("terraform",tmp_dir)
+ zfile.close
+ if os.path.isfile(tmp_dir+"/terraform"):
+   if os.path.isfile(tf_dir+"/terraform"): os.remove(tf_dir+"/terraform")
+   os.rename(tmp_dir+"/terraform",tfp_lfile)
+   os.chmod(tfp_lfile, 0o755)
+   os.symlink(tfp_lfile,tf_dir+"/terraform")
 
